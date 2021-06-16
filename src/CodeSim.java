c57bf85aae32365bfe20ffc35f422aed3d0b0f63
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-06-16 23:45:08 +0800

    can now customize appearance

--- src/CodeSim.java
+++ src/CodeSim.java
@@ -7,28 +7,35 @@ import java.util.Scanner;
 
 class Page
 {
-    private JPanel panel;
-    private ArrayList<JPanel> lines;
+    private final JPanel parent;
+    private final ArrayList<JPanel> linePanels;
+    private final ArrayList<JLabel> lineLabels;
 
-    public Page(JPanel panel, ArrayList<JPanel> lines)
+    public Page(JPanel panel, ArrayList<JPanel> linePanels, ArrayList<JLabel> lineLabels)
     {
-        this.panel = panel;
-        this.lines = lines;
+        this.parent = panel;
+        this.linePanels = linePanels;
+        this.lineLabels = lineLabels;
     }
 
-    public JPanel getPanel()
+    public JPanel getParent()
     {
-        return panel;
+        return parent;
     }
 
-    public JPanel getLine(int i)
+    public JPanel getPanel(int i)
     {
-        return lines.get(i);
+        return linePanels.get(i);
+    }
+
+    public JLabel getLabels(int i)
+    {
+        return lineLabels.get(i);
     }
 
     public int size()
     {
-        return lines.size();
+        return linePanels.size();
     }
 }
 
@@ -39,7 +46,6 @@ public class CodeSim extends JPanel
         JFrame frame = new JFrame("CodeSim");
         frame.setLocationRelativeTo(null);
         frame.setSize(500, 300);
-        frame.setMinimumSize(frame.getSize());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         CodeSim codeSim = new CodeSim(new String[]{"samplecode", "samplecode1"});
         codeSim.init();
@@ -52,8 +58,24 @@ public class CodeSim extends JPanel
     private final ArrayList<Page> pages;
     private final CardLayout cardLayout;
 
+    // Display Attributes
+    private Font font;
+    private Color lineNumberBackground;
+    private Color lineNumberForeground;
+    private Color background;
+    private Color foreground;
+    private Color highlightBackground;
+    private Color highlightForeGround;
+
     private CodeSim()
     {
+        font = new Font("Consola", Font.PLAIN, 13);
+        lineNumberBackground = Color.black;
+        lineNumberForeground = Color.gray;
+        background = Color.white;
+        foreground = Color.black;
+        highlightBackground = Color.cyan;
+        highlightForeGround = Color.black;
         codes = new ArrayList<>();
         pages = new ArrayList<>();
         cardLayout = new CardLayout();
@@ -97,27 +119,31 @@ public class CodeSim extends JPanel
             rowViewport.add(rowHeaderPanel);
             scrollPane.setRowHeader(rowViewport);
 
-            ArrayList<JPanel> lines = new ArrayList<>();
+            ArrayList<JPanel> panels = new ArrayList<>();
+            ArrayList<JLabel> labels = new ArrayList<>();
             for(int i = 0; i < page.size(); i++)
             {
                 JPanel number = new JPanel(new FlowLayout((FlowLayout.RIGHT)));
-                number.setBackground(Color.black);
-                number.setPreferredSize(new Dimension(20, 20));
+                number.setBackground(lineNumberBackground);
+                number.setPreferredSize(new Dimension(20, 25));
                 JLabel numberLabel = new JLabel(String.valueOf(i + 1));
-                numberLabel.setForeground(Color.GRAY);
-                numberLabel.setFont(new Font("Consolas", Font.PLAIN, 12));
+                numberLabel.setForeground(lineNumberForeground);
+                numberLabel.setFont(font);
                 number.add(numberLabel);
                 rowHeaderPanel.add(number);
 
                 JPanel linePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-                linePanel.setPreferredSize(new Dimension(0, 20));
+                linePanel.setBackground(background);
+                linePanel.setPreferredSize(new Dimension(0, 25));
                 JLabel lineLabel = page.get(i);
-                lineLabel.setFont(new Font("Consolas", Font.PLAIN, 12));
+                lineLabel.setForeground(foreground);
+                lineLabel.setFont(font);
                 linePanel.add(lineLabel);
                 pagePanel.add(linePanel);
-                lines.add(linePanel);
+                panels.add(linePanel);
+                labels.add(lineLabel);
             }
-            pages.add(new Page(pagePanel, lines));
+            pages.add(new Page(pagePanel, panels, labels));
             add(scrollPane);
         }
     }
@@ -138,16 +164,17 @@ public class CodeSim extends JPanel
         {
             for(int i = 0; i < page.size(); i++)
             {
-                JPanel line = page.getLine(i);
+                JPanel panel = page.getPanel(i);
+                JLabel label = page.getLabels(i);
                 if(i == index)
                 {
-                    line.setBackground(Color.CYAN);
-                    line.setForeground(Color.WHITE);
+                    panel.setBackground(highlightBackground);
+                    label.setForeground(highlightForeGround);
                 }
                 else
                 {
-                    line.setBackground(Color.WHITE);
-                    line.setForeground(Color.BLACK);
+                    panel.setBackground(background);
+                    label.setForeground(foreground);
                 }
             }
         }
@@ -174,5 +201,83 @@ public class CodeSim extends JPanel
             System.err.println("Path not found: " + path);
         }
         return null;
+    }
+
+    // Setters
+    public void setFont(String name)
+    {
+        font = new Font(name, Font.PLAIN, 13);
+    }
+
+    public void setLineNumberBackground(Color lineNumberBackground)
+    {
+        this.lineNumberBackground = lineNumberBackground;
+    }
+
+    public void setLineNumberForeground(Color lineNumberForeground)
+    {
+        this.lineNumberForeground = lineNumberForeground;
+    }
+
+    @Override
+    public void setBackground(Color background)
+    {
+        this.background = background;
+    }
+
+    @Override
+    public void setForeground(Color foreground)
+    {
+        this.foreground = foreground;
+    }
+
+    public void setHighlightBackground(Color highlightBackground)
+    {
+        this.highlightBackground = highlightBackground;
+    }
+
+    public void setHighlightForeGround(Color highlightForeGround)
+    {
+        this.highlightForeGround = highlightForeGround;
+    }
+
+    // Getters
+
+    @Override
+    public Font getFont()
+    {
+        return font;
+    }
+
+    public Color getLineNumberBackground()
+    {
+        return lineNumberBackground;
+    }
+
+    public Color getLineNumberForeground()
+    {
+        return lineNumberForeground;
+    }
+
+    @Override
+    public Color getBackground()
+    {
+        return background;
+    }
+
+    @Override
+    public Color getForeground()
+    {
+        return foreground;
+    }
+
+    public Color getHighlightBackground()
+    {
+        return highlightBackground;
+    }
+
+    public Color getHighlightForeGround()
+    {
+        return highlightForeGround;
     }
 }
